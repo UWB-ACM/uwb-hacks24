2a6e307f6182147f78dc2a4a6bafd46d313cf987
Vikrant Verma <87392159+crayzeerr@users.noreply.github.com>
2023-03-20 13:01:56 -0700

    added particle thing, linked contact btn, added faq and track subheadings

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import './App.css';
 import Header from "./Header";
 import CountdownTimer from "./CountDownTimer";
 import Tracks from "./Tracks";
+import Particles from "react-particles";
+import options from "./particleConfig"; // configuration of particles
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+import { loadTrianglesPreset } from "tsparticles-preset-triangles";
+import FAQ from "./FAQ";
 
 function App() {
+
+  const particlesInit = useCallback(async (engine) => {
+      await loadFull(engine);
+  }, [])
+
+
+
   return (
     <div className="App">
         <Header />
         <CountdownTimer />
         <Tracks />
+        <FAQ />
+        <Particles className="particles" options={options} init={particlesInit} />
     </div>
+
   );
 }
 
---
+++ src/FAQ.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function FAQ() {
+        return (
+            <div id="repulse-div" className="faq">
+                <div id="repulse-div" className="faqHeader">
+                    FAQ
+                </div>
+            </div>
+        );
+}
+
+export default FAQ;
