7399c72b3f5ba99d2af1fb5e960d37c47a32e787
anthonyhucsse <anthonyhuCSSE@gmail.com>
2023-03-29 13:41:17 -0700

    added new font for experimentation

--- src/Home/Tracks.js
+++ src/Home/Tracks.js
@@ -4,7 +4,7 @@ import './Home.css';
 function Tracks() {
     return (
         <div className="h-80 bg-white rounded-xl shadow-md overflow-hidden border-4 border-yellow-600 rounded-lg mx-5 my-5">
-            <div className="font-bold text-4xl text-center">
+            <div className="font-SecularOne text-6xl text-center mt-3 mb-2">
                 Tracks
             </div>
         </div>
