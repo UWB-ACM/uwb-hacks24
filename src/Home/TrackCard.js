172c9d0d82a23e3a9aa952e84b62d5f114379506
anthonyhucsse <anthonyhuCSSE@gmail.com>
2023-03-30 00:23:01 -0700

    added flipping card for tracks

---
+++ src/Home/TrackCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Home.css';
+
+function TrackCard(props) {
+    return (
+      <div className="items-center justify-center bg-slate-100 pt-2 pb-3 pl-4 pr-4">
+        <div className="group h-96 w-80 [perspective:1000px]">
+          <div className="border-1 rounded-xl relative h-full w-full rounded-xltransition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+            <div className="absolute inset-0">
+              <img className="h-full w-full rounded-xl object-contain opacity-50" src={props.backgroundImage} alt="" />
+            </div>
+            <div className="absolute inset-0 h-full w-full rounded-xl [backface-visibility:hidden]">
+              <div className="flex flex-col items-center justify-center h-full w-full bg-white rounded-xl p-4">
+              <img className="h-full w-full rounded-xl object-contain " src={props.backgroundImage} alt="" />
+              </div>
+            </div>
+            <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+              <div className="flex min-h-full flex-col items-center justify-center">
+                <h2 className="font-bold text-3xl">{props.cardTitle}</h2>
+                <div className="text-1xl">{props.children}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+
+export default TrackCard
--- src/Home/Tracks.js
+++ src/Home/Tracks.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Home.css';
+import TrackCard from "./TrackCard"
+import Entertainment from "./placeholder images/entertainment.png"
+import Sustainability from "./placeholder images/sustainability.png"
+import Education from "./placeholder images/education.png"
 
 function Tracks() {
     return (
-        <div className="h-80 bg-white rounded-xl shadow-md overflow-hidden border-4 border-yellow-600 rounded-lg mx-5 my-5">
+        <div className="rounded-xl overflow-hidden  rounded-lg my-5">
             <div className="font-SecularOne text-6xl text-center mt-3 mb-2">
                 Tracks
+            </div>
+            <div className="flex flex-col items-center justify-center sm:flex-row sm:flex-wrap overflow-hidden">
+                <TrackCard cardTitle="Sustainability" backgroundImage={Sustainability}> As we start to move away from quarantine, how can we ensure people are being environmentally conscious when engaging in outdoor activities? Look into ways sustainability can be maintained now that gatherings and events are moving in-person. </TrackCard>
+                <TrackCard cardTitle="Education" backgroundImage={Education}> Education was moved online during quarantine causing many to adjust their way of learning for their new environment. As society moves back to learning in-person, how can we help assimilate people back to learning in-person? </TrackCard>
+                <TrackCard cardTitle="Entertainment" backgroundImage={Entertainment}> Entertainment was majorly affected by the pandemic resulting in people having to find sources of enjoyment through different avenues that worked under the quarantine. Now that quarantine is over, how can we help people transition and find in-person entertainment again? </TrackCard>
             </div>
         </div>
     );
