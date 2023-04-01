5fbc1e149c24213554b07accfc13e838a4c4c041
anthonyhucsse <anthonyhuCSSE@gmail.com>
2023-03-31 23:40:55 -0700

    css changes

--- src/Home/Contact.js
+++ src/Home/Contact.js
@@ -29,22 +29,23 @@ export const ContactUs = () => {
             <Particles className="particles" options={options} init={particlesInit} />
             <div className="flex shadow-md overflow-hidden border-1 rounded-xl mx-auto mt-5 flex-col sm:w-1/2 bg-light-purple">
                 <form ref={form} onSubmit={sendEmail} className="w-full px-5 py-3">
-                    <label htmlFor="name" className="font-SecularOne block text-3xl font-medium leading-6 text-white">
+                <div className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-1 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"> CONTACT FORM </div>
+                    <label htmlFor="name" className="font-SecularOne block text-3xl font-medium leading-6 text-white  pt-3">
                         Name
                     </label>
                     <input type="text" name="name" className="relative mt-2 rounded-md shadow-sm border-1 text-xl w-full"/>
 
-                    <label htmlFor="email" className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-5">
+                    <label htmlFor="email" className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-4">
                         Email
                     </label>
                     <input type="email" name="user_email" className="relative mt-2 rounded-md shadow-sm border-1 text-xl w-full"/>
 
-                    <label htmlFor="message" className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-5">
+                    <label htmlFor="message" className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-4">
                         Message
                     </label>
                     <textarea name="message" className="relative mt-2 rounded-md shadow-sm border-1 text-xl w-full h-40"/>
 
-                    <div className="pt-3">
+                    <div className="pt-4 pb-2">
                         <button
                             className="bg-white text-blue-700 font-semibold py-2 px-4 rounded w-full">
                             <input type="submit" value="Send" className="text-2xl"/>
