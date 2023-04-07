d4baecf6ac961c7406d8c557a757523ee2b4d41b
anthonyhucsse <anthonyhuCSSE@gmail.com>
2023-04-07 16:10:40 -0700

    home nav bar moves with page now

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -111,7 +111,7 @@ export default function Header() {
             </nav>
 
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 border-b-1" />
+                <div className="fixed inset-0 " />
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
