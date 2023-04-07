2e8526393d1b1f101ceeaf02266040c722ba250a
anthonyhucsse <anthonyhuCSSE@gmail.com>
2023-04-07 16:10:02 -0700

    home nav header moves with page now

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -33,7 +33,7 @@ export default function Header() {
     if (location.pathname === "/uwb-hacks23/Contact" || location.pathname === "/uwb-hacks23/Registration" || location.pathname === "/uwb-hacks23/Login") {
         return (
             <>
-                <header className="h-20 flex justify-end">
+                <header className="h-20 flex justify-end ">
                     <div className="px-5 py-4">
                         {!isHovered ? (
                             <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
@@ -67,8 +67,8 @@ export default function Header() {
     }
 
     return (
-        <header className="bg-white">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 h-20" aria-label="Global">
+        <header className="bg-white sticky top-0 z-20 ">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 h-20 " aria-label="Global">
                 <div className="flex lg:flex-1  h-full">
                     <span className="sr-only">UWB Hacks</span>
                     <img className="h-full" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="UWB Hacks Logo" />
@@ -111,7 +111,7 @@ export default function Header() {
             </nav>
 
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-10" />
+                <div className="fixed inset-0 border-b-1" />
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
