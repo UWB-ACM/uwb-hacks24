ad9070de84795df0504ff4aa56593022df6521ff
anthonyhucsse <anthonyhuCSSE@gmail.com>
2023-04-07 16:53:37 -0700

    fixed bug with mobile menu close button

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -123,7 +123,7 @@ export default function Header() {
 
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                 <div className="fixed inset-0 " />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-20 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
@@ -135,11 +135,11 @@ export default function Header() {
                         </a>
                         <button
                             type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700 hover:bg-light-purple"
                             onClick={() => setMobileMenuOpen(false)}
                         >
                             <span className="sr-only">Close menu</span>
-                            <XMarkIcon className="h-6 w-6 hover:bg-light-purple rounded" aria-hidden="true" />
+                            <XMarkIcon className="h-6 w-6  rounded" aria-hidden="true" />
                         </button>
                     </div>
                     <div className="mt-6 flow-root">
