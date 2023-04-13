05d99a5d29740262627a8f92336f8b61c9fb941b
anthonyhucsse <anthonyhuCSSE@gmail.com>
2023-04-13 00:39:21 -0700

    css changes to contact form

--- src/Home/Contact.js
+++ src/Home/Contact.js
@@ -27,27 +27,27 @@ export const ContactUs = () => {
     return (
         <div className="overflow-hidden flex flex-col items-center justify-center">
             <Particles className="particles" options={options} init={particlesInit} />
-            <div className="flex shadow-md overflow-hidden border-1 rounded-xl mx-auto mt-5 flex-col sm:w-1/2 bg-light-purple">
+            <div className="flex shadow-md overflow-hidden border-2 rounded-xl mx-auto my-5 flex-col sm:w-1/2 bg-light-purple">
                 <form ref={form} onSubmit={sendEmail} className="w-full px-5 py-3">
                     <div className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-1 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] mb-3 text-center"> CONTACT FORM </div>
-                    <label htmlFor="name" className="font-SecularOne block text-3xl font-medium leading-6 text-white  pt-3">
+                    <label htmlFor="name" className="font-SecularOne block text-2xl  leading-6 text-white  pt-3">
                         Name
                     </label>
-                    <input type="text" name="name" className="relative mt-2 rounded-md shadow-sm border-1 text-xl w-full"/>
+                    <input type="text" name="name" className="relative mt-1 rounded-md shadow-sm border-1 text-xl w-full"/>
 
-                    <label htmlFor="email" className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-4">
+                    <label htmlFor="email" className="font-SecularOne block text-2xl  leading-6 text-white pt-4">
                         Email
                     </label>
-                    <input type="email" name="user_email" className="relative mt-2 rounded-md shadow-sm border-1 text-xl w-full"/>
+                    <input type="email" name="user_email" className="relative mt-1 rounded-md shadow-sm border-1 text-xl w-full"/>
 
-                    <label htmlFor="message" className="font-SecularOne block text-3xl font-medium leading-6 text-white pt-4">
+                    <label htmlFor="message" className="font-SecularOne block text-2xl  leading-6 text-white pt-4">
                         Message
                     </label>
-                    <textarea name="message" className="relative mt-2 rounded-md shadow-sm border-1 text-xl w-full h-40"/>
+                    <textarea name="message" className="relative mt-1 rounded-md shadow-sm border-1 text-xl w-full h-40"/>
 
-                    <div className="pt-4 pb-2">
+                    <div className="pt-4 pb-2 ">
                         <button
-                            className="bg-white text-blue-700 font-semibold py-2 px-4 rounded w-full">
+                            className="bg-white text-blue-700 font-semibold py-2 px-4 rounded w-full ">
                             <input type="submit" value="Send" className="text-2xl"/>
                         </button>
                     </div>
