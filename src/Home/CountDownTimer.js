fbe1d213e5e0d30df9a7244ccf45840cd38f8b88
Natha Chiu <81452446+MaranathaC@users.noreply.github.com>
2023-04-30 21:50:07 -0700

    add new timer

--- src/Home/CountDownTimer.js
+++ src/Home/CountDownTimer.js
@@ -4,18 +4,24 @@ import Marquee from "react-fast-marquee";
 
 function CountdownTimer() {
     const [countdown, setCountdown] = useState(Date.parse('2023-05-05T00:00:00') - Date.now());
+    const [endTimer, setEndTimer] = useState(Date.parse('2023-05-07T11:00:00') - Date.now());
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCountdown(countdown - 1000);
+            setEndTimer(endTimer - 1000);
         }, 1000);
 
         return () => clearInterval(interval);
-    }, [countdown]);
+    }, [countdown, endTimer]);
 
     const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
     const hours = Math.floor((countdown / (1000 * 60 * 60)) % 24);
     const minutes = Math.floor((countdown / (1000 * 60)) % 60);
+
+    const endDays = Math.floor(endTimer / (1000 * 60 * 60 * 24));
+    const endHours = Math.floor((endTimer / (1000 * 60 * 60)) % 24);
+    const endMinutes = Math.floor((endTimer / (1000 * 60)) % 60);
 
     const [playMarquee, setPlayMarquee] = useState(true);
 
@@ -29,10 +35,27 @@ function CountdownTimer() {
     useEffect(() => {
 
     }, [playMarquee]);
+
+    if (countdown < 0) {
+        if (endTimer < 0) {
+            return (
+                <div className="countdown-timer-box py-2">
+                    <Marquee play={playMarquee} gradient={false} speed={Math.log(window.screen.width) *10} onCycleComplete={handleMarquee}>Hacking Ends! Thank You! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Marquee>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className="countdown-timer-box py-2">
+                    <Marquee play={playMarquee} gradient={false} speed={Math.log(window.screen.width) *10} onCycleComplete={handleMarquee}>{endDays} days, {endHours} hours, {endMinutes} minutes until Hacking ends! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Marquee>
+                </div>
+            );
+        }
+    }
+
     return (
         <div className="countdown-timer-box py-2">
-                        <Marquee play={playMarquee} gradient={false} speed={Math.log(window.screen.width) *10} onCycleComplete={handleMarquee}>{days} days, {hours} hours, {minutes} minutes until Hacking begins! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Marquee>
-
+            <Marquee play={playMarquee} gradient={false} speed={Math.log(window.screen.width) *10} onCycleComplete={handleMarquee}>{days} days, {hours} hours, {minutes} minutes until Hacking begins! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Marquee>
         </div>
     );
 }
