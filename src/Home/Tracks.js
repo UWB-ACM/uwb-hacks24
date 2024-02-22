fe50af92d27431586effc5c62051f7bcdedf0df8
alan.lai1738 <youremail@example.com>
2024-02-22 15:17:13 -0800

    Splash text color added

--- src/Home/Tracks.js
+++ src/Home/Tracks.js
@@ -14,13 +14,13 @@ function Tracks() {
                 </div>
                 
                 <div className="font-stacker text-pink flex flex-col items-center justify-center sm:flex-row sm:flex-wrap overflow-hidden pb-36">
-                    <StackedCards cardTitle="BioTech" backgroundImage={BioTech}>As technology and healthcare continue to advance, explore how <b className="text-cyan">BioTech</b> can play a pivotal role in enhancing the medical field</StackedCards>
+                    <StackedCards cardTitle="BioTech" backgroundImage={BioTech}>As technology and healthcare continue to advance, explore how <b className="text-cyan">null</b> can play a pivotal role in enhancing the medical field</StackedCards>
 
 
-                    <StackedCards cardTitle="Business" backgroundImage={Business}>As companies integrate AI into their workforce, let's explore potential strategies to streamline processes and enhance overall <b className="text-cyan">Business</b> efficiency</StackedCards>
+                    <StackedCards cardTitle="Business" backgroundImage={Business}>As companies integrate AI into their workforce, let's explore potential strategies to streamline processes and enhance overall <b className="text-cyan">null</b> efficiency</StackedCards>
 
 
-                    <StackedCards cardTitle="Social Good" backgroundImage={Social}>Delve into strategies to utilize AI for community initiatives, champion inclusivity, and catalyze profound, positive change in the pursuit of <b className="text-cyan">Social Good</b></StackedCards>
+                    <StackedCards cardTitle="Social Good" backgroundImage={Social}>Delve into strategies to utilize AI for community initiatives, champion inclusivity, and catalyze profound, positive change in the pursuit of <b className="text-cyan">null</b></StackedCards>
 
                 </div>
             </div>
