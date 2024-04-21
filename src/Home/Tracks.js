1f9cf4dbb4b9715bed422e8eeb99a1884fd89860
alan.lai1738 <youremail@example.com>
2024-04-20 21:43:44 -0700

    Tracks release

--- src/Home/Tracks.js
+++ src/Home/Tracks.js
@@ -1,50 +1,55 @@
 import React from 'react';
 import './Home.css';
-import Social from "./media/socialGood.png"
 import BioTech from "./media/biotech.png"
 import Business from "./media/business.png"
+import Education from "./media/education.png"
+import Entertainment from "./media/entertainment.png"
 import StackedCards from './TrackCard';
-
-// function Tracks() {
-//     return (
-//         <>
-//             <div className="bg-purple rounded-xl overflow-hidden" id="trackSection">
-//                 <div className="font-stacker text-pink text-6xl text-center mt-3 mb-2">
-//                     Tracks
-//                 </div>
-
-//                 <div className="font-stacker text-pink flex flex-col items-center justify-center sm:flex-row sm:flex-wrap overflow-hidden pb-36">
-//                     <StackedCards cardTitle="BioTech" backgroundImage={BioTech}>As technology and healthcare continue to advance, explore how <b className="text-cyan">null</b> can play a pivotal role in enhancing the medical field</StackedCards>
-
-
-//                     <StackedCards cardTitle="Business" backgroundImage={Business}>As companies integrate AI into their workforce, let's explore potential strategies to streamline processes and enhance overall <b className="text-cyan">null</b> efficiency</StackedCards>
-
-
-//                     <StackedCards cardTitle="Social Good" backgroundImage={Social}>Delve into strategies to utilize AI for community initiatives, champion inclusivity, and catalyze profound, positive change in the pursuit of <b className="text-cyan">null</b></StackedCards>
-
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
-
-
 
 function Tracks() {
     return (
         <>
-            <div className="bg-purple overflow-hidden" id="trackSection">
+            <div className="bg-purple rounded-xl overflow-hidden" id="trackSection">
                 <div className="font-stacker text-pink text-6xl text-center mt-3 mb-2">
                     Tracks
                 </div>
 
                 <div className="font-stacker text-pink flex flex-col items-center justify-center sm:flex-row sm:flex-wrap overflow-hidden pb-36">
-                    <StackedCards cardTitle="Soon..." backgroundImage={BioTech}>Tracks to be released soon. Stay tuned.</StackedCards>
+                    <StackedCards cardTitle="BioTech" backgroundImage={BioTech}>AI transforms healthtech & biotech by optimizing drug discovery, diagnostics, precision medicine, and healthcare operations. It analyzes vast datasets to predict disease risks, personalize treatments, and enhance patient outcomes.</StackedCards>
+
+
+                    <StackedCards cardTitle="Business" backgroundImage={Business}>AI revolutionizes business by automating tasks, personalizing customer experiences, optimizing operations, predicting trends, managing risks, and enhancing HR processes. It drives efficiency, innovation, and competitiveness across industries.</StackedCards>
+
+
+                    <StackedCards cardTitle="Education" backgroundImage={Education}>AI enhances education by personalizing learning, enhancing teaching, analyzing data, improving accessibility, innovating content, and fostering lifelong learning.</StackedCards>
+
+                    <StackedCards cardTitle="Entertainment" backgroundImage={Entertainment}>AI provides entertainment with personalized content, immersive experiences, efficient production, audience engagement, predictive analytics, recommendation systems, content moderation, and creative collaboration, shaping the future of digital entertainment.
+</StackedCards>
+                    
+
                 </div>
             </div>
         </>
     );
 }
+
+
+
+// function Tracks() {
+//     return (
+//         <>
+//             <div className="bg-purple overflow-hidden" id="trackSection">
+//                 <div className="font-stacker text-pink text-6xl text-center mt-3 mb-2">
+//                     Tracks
+//                 </div>
+
+//                 <div className="font-stacker text-pink flex flex-col items-center justify-center sm:flex-row sm:flex-wrap overflow-hidden pb-36">
+//                     <StackedCards cardTitle="Soon..." backgroundImage={BioTech}>Tracks to be released soon. Stay tuned.</StackedCards>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
 
 
 export default Tracks;
